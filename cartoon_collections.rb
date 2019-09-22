edb8c5850c87a58b7f610bf5a101eb7f800a993b
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-22 20:42:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,9 +4,9 @@ end
 
 def summon_captain_planet(array)
    array.map! {|name| name.capitalize}
-   array.split("! ")
+   #array.split("! ")
   end
-  #array
+  array
 end
 
 def long_planeteer_calls# code an argument here
