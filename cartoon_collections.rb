7562f12d27599a6b0313dc9275b5e5f26f72d2fe
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-22 21:18:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,8 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese(cheese_array)
+def find_the_cheese(cheese_bascet)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  cheese_bascet
   
 end
