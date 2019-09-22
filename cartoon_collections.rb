d4b3845068f1d1f39207e5aaedc53f5a3626ea80
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-22 20:35:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves# code an argument here
 end
 
 def summon_captain_planet(array)
-  array.map! {|name| name.capitalize}
+  array.map! {|name| name.capitalize } 
   #i = 0
   #array_new = []
   #while i < array.length
