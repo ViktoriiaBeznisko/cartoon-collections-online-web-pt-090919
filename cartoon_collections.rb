e87a0f019af96e7a8b2a7ded60d015695bca2ed8
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-22 21:04:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
+  dwarves.each_with_index {|dwar, index| puts "#{index +1}. #{dwar}"}
 end
 
 def summon_captain_planet(array)
